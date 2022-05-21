46223777a91cb4520717791129148179a9c506da
Lorenzo Rivosecchi <lorenzo.rivosecchi@lacolonia.studio>
2022-05-21 14:30:41 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import type { Component } from "solid-js";
+
+import styles from "./App.module.css";
+
+const App: Component = () => {
+  return (
+    <div class={styles.App}>
+      <h1>Gabriele Ferrarini</h1>
+    </div>
+  );
+};
+
+export default App;
