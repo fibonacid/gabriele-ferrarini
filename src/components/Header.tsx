85fba3c1d0a72c9cecd7bac8f551f21128afed36
Lorenzo Rivosecchi <lorenzo.rivosecchi@lacolonia.studio>
2022-10-23 12:20:51 +0200

    add toggle

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,19 @@
-import type { Component } from "solid-js";
+import { Component, createSignal } from "solid-js";
+
+const [menuOpen, setMenuOpen] = createSignal(false);
 
 const Header: Component = () => {
   return (
     <>
-      <header class="fixed  left-0 right-0 top-0 h-header z-20 flex items-center p-3 bg-white">
-        <h1 class="text-3xl">Gabriele Ferrarini</h1>
+      <header class="fixed left-0 right-0 top-0 h-header z-20 flex items-center bg-white text-3xl justify-between">
+        <h1 class="px-3">Gabriele Ferrarini</h1>
+        <div
+          class="px-3"
+          role="button"
+          onClick={() => setMenuOpen(!menuOpen())}
+        >
+          {menuOpen() ? "+" : "-"}
+        </div>
       </header>
       <div class="fixed z-10 top-header left-0 right-0 bottom-0 transform -translate-y-full"></div>
     </>
