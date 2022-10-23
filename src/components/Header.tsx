f803510726ce72de5b92ac8c72b4ffb218ac7b3a
Lorenzo Rivosecchi <lorenzo.rivosecchi@lacolonia.studio>
2022-10-23 22:46:44 +0200

    keep nav open on homepage

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,14 @@
-import type { Component } from "solid-js";
 import { HeaderNav, HeaderNavProps } from "./HeaderNav";
-import { setMenuOpen, menuOpen } from "./signals";
+import { Component, createSignal } from "solid-js";
 
 export type HeaderProps = {
   label: string;
-  items: HeaderNavProps["items"];
+  navItems: HeaderNavProps["items"];
+  navIsOpen?: boolean;
 };
 
 const Header: Component<HeaderProps> = (props) => {
+  const [navIsOpen, setNavIsOpen] = createSignal(props.navIsOpen || false);
   return (
     <>
       <header class="fixed left-0 right-0 top-0 h-header z-20 flex items-center bg-white text-3xl justify-between border-b-2 border-black">
@@ -15,12 +16,12 @@ const Header: Component<HeaderProps> = (props) => {
         <div
           class="px-3"
           role="button"
-          onClick={() => setMenuOpen(!menuOpen())}
+          onClick={() => setNavIsOpen(!navIsOpen())}
         >
-          {menuOpen() ? "-" : "+"}
+          {navIsOpen() ? "-" : "+"}
         </div>
       </header>
-      <HeaderNav items={props.items} />
+      <HeaderNav items={props.navItems} isOpen={navIsOpen()} />
     </>
   );
 };
--- src/components/HeaderNav.tsx
+++ src/components/HeaderNav.tsx
@@ -1,5 +1,4 @@
 import { Component, For } from "solid-js";
-import { menuOpen } from "./signals";
 import { classes } from "./utils";
 
 export type HeaderNavItem = {
@@ -9,26 +8,25 @@ export type HeaderNavItem = {
 
 export type HeaderNavProps = {
   items: HeaderNavItem[];
+  isOpen: boolean;
 };
 
-export const HeaderNav: Component<HeaderNavProps> = (props) => {
-  return (
-    <nav
-      class={classes(
-        "fixed z-10 top-header left-0 right-0 transition-transform ",
-        menuOpen() || "transform -translate-y-full"
+export const HeaderNav: Component<HeaderNavProps> = (props) => (
+  <nav
+    class={classes(
+      "fixed z-10 top-header left-0 right-0 transition-transform ",
+      props.isOpen || "transform -translate-y-full"
+    )}
+  >
+    <For each={props.items}>
+      {(item) => (
+        <a
+          class="px-3 h-header flex items-center bg-white text-3xl justify-between border-b-2 border-black"
+          href={item.href}
+        >
+          {item.label}
+        </a>
       )}
-    >
-      <For each={props.items}>
-        {(item) => (
-          <a
-            class="px-3 h-header flex items-center bg-white text-3xl justify-between border-b-2 border-black"
-            href={item.href}
-          >
-            {item.label}
-          </a>
-        )}
-      </For>
-    </nav>
-  );
-};
+    </For>
+  </nav>
+);
