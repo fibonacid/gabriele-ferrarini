94d34653655e9ba89b96654f5d2c71956b5aba18
Lorenzo Rivosecchi <lorenzo.rivosecchi@lacolonia.studio>
2022-10-23 12:14:29 +0200

    create header and menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,12 @@ import type { Component } from "solid-js";
 
 const Header: Component = () => {
   return (
-    <header class="h-12 flex items-center p-3">
-      <h1 class="text-3xl">Gabriele Ferrarini</h1>
-    </header>
+    <>
+      <header class="fixed  left-0 right-0 top-0 h-header z-20 flex items-center p-3 bg-white">
+        <h1 class="text-3xl">Gabriele Ferrarini</h1>
+      </header>
+      <div class="fixed z-10 top-header left-0 right-0 bottom-0 transform -translate-y-full"></div>
+    </>
   );
 };
 
