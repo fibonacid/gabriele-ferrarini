82e6b8433be82d9edaab1dd5bfc4cd4fb27de146
Lorenzo Rivosecchi <lorenzo.rivosecchi@lacolonia.studio>
2022-10-23 12:03:46 +0200

    install solid and create header

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import type { Component } from "solid-js";
+
+const Header: Component = () => {
+  return (
+    <header class="h-12 flex items-center p-3">
+      <h1 class="text-3xl">Gabriele Ferrarini</h1>
+    </header>
+  );
+};
+
+export default Header;
