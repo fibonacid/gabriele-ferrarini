1c6e0d089fbd409fb84798487bbdaa62d210b6dc
Lorenzo Rivosecchi <lorenzo.rivosecchi@lacolonia.studio>
2022-11-01 17:03:50 +0100

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,14 +7,20 @@ import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [
     tailwind({
       // Example: Disable injecting a basic `base.css` import on every page.
       // Useful if you need to define and/or import your own custom `base.css`.
-      config: { applyBaseStyles: false },
+      config: {
+        applyBaseStyles: false,
+      },
     }),
     solidJs(),
   ],
+  adapter: vercel(),
 });
